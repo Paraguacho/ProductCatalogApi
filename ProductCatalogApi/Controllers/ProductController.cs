ef2a31544c15f24253ad9df6e4ab4a5f69177dff
Paraguacho <sebastiansoq03@hotmail.com>
2026-01-29 20:40:28 -0700

    Cambio de nombre

--- ProductCatalogApi/Controllers/ProductController.cs
+++ ProductCatalogApi/Controllers/ProductController.cs
@@ -12,11 +12,11 @@ namespace ProductCatalogApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
-        public ProductsController(ApplicationDbContext context)
+        public ProductController(ApplicationDbContext context)
         {
             _context = context;
         }
